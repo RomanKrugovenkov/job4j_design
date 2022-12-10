eca4b67c3ba7ca17e5f52bd70cf286edf492e110
krugo <krugovenkov@mail.ru>
2022-12-10 19:51:55 +0300

    7. ListIterator [#350217]

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -1,6 +1,8 @@
 package ru.job4j.iterator;
 
-import java.util.*;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public class ListUtils {
@@ -18,18 +20,45 @@ public class ListUtils {
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
-
+        Objects.checkIndex(index + 1, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index + 1) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
-
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.remove();
+            }
+        }
     }
 
     public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
-
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.set(value);
+            }
+        }
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
-
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            T it = iterator.next();
+            for (T el : elements) {
+                if (it.equals(el)) {
+                    iterator.remove();
+                    break;
+                }
+            }
+        }
     }
 }
--- src/test/java/ru/job4j/iterator/ListUtilsTest.java
+++ src/test/java/ru/job4j/iterator/ListUtilsTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.*;
+import java.util.function.Predicate;
 
 class ListUtilsTest {
 
@@ -31,5 +32,29 @@ class ListUtilsTest {
     void whenAddAfter() {
         ListUtils.addAfter(input, 0, 2);
         assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenRemoveIf() {
+        ListUtils.addAfter(input, 0, 2);
+        Predicate<Integer> filter = x -> x % 2 != 0;
+        ListUtils.removeIf(input, filter);
+        assertThat(input).hasSize(1).containsSequence(2);
+    }
+
+    @Test
+    void whenReplaceIf() {
+        ListUtils.addAfter(input, 0, 2);
+        Predicate<Integer> filter = x -> x % 2 == 0;
+        ListUtils.replaceIf(input, filter, 99);
+        assertThat(input).hasSize(3).containsSequence(1, 99, 3);
+    }
+
+    @Test
+    void whenRemoveAll() {
+        ListUtils.addAfter(input, 0, 2);
+        List<Integer> elements = Arrays.asList(1, 2);
+        ListUtils.removeAll(input, elements);
+        assertThat(input).hasSize(1).containsSequence(3);
     }
 }
