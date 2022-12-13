ece76b50249006d9e8a98232d6c2508f8edbb84b
krugo <krugovenkov@mail.ru>
2022-12-13 11:57:31 +0300

    7. ListIterator [#350217]

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -38,8 +38,6 @@ public class ListUtils {
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
-        for (T el : elements) {
-            removeIf(list, i -> i.equals(el));
-        }
+        removeIf(list, elements::contains);
     }
 }
