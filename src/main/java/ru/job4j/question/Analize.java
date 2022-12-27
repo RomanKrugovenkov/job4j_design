7f694adb62ffed4496cec6ad2fba9ca7f89bd7c4
krugo <krugovenkov@mail.ru>
2022-12-27 21:38:08 +0300

    2. Статистика по коллекции. [#45889]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -1,34 +1,33 @@
 package ru.job4j.question;
 
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Objects;
+import java.util.Map;
 import java.util.Set;
 
 public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
         Info rsl = new Info(0, 0, 0);
-        Set<User> tempPrev = new HashSet<>(previous);
+        Map<Integer, String> mapPrev = new HashMap<>();
+        for (var user : previous) {
+            mapPrev.put(user.getId(), user.getName());
+        }
         Set<User> tempCur = new HashSet<>(current);
-        tempPrev.removeAll(current);
-        tempCur.removeAll(previous);
-        var itPrev = tempPrev.iterator();
         var itCur = tempCur.iterator();
-        while (itPrev.hasNext()) {
-            var userPr = itPrev.next();
-            while (itCur.hasNext()) {
-                var userCur = itCur.next();
-                if (Objects.equals(userPr, userCur)) {
-                    itCur.remove();
-                    itPrev.remove();
-                } else if (userPr.getId() == userCur.getId()) {
-                    rsl.setChanged(rsl.getChanged() + 1);
-                    itPrev.remove();
-                    itCur.remove();
-                }
+        while (itCur.hasNext()) {
+            var userCur = itCur.next();
+            if (mapPrev.containsKey(userCur.getId())
+            && mapPrev.containsValue(userCur.getName())) {
+                mapPrev.remove(userCur.getId());
+                itCur.remove();
+            } else if (mapPrev.containsKey(userCur.getId())) {
+                mapPrev.remove(userCur.getId());
+                itCur.remove();
+                rsl.setChanged(rsl.getChanged() + 1);
             }
         }
-        rsl.setDeleted(tempPrev.size());
+        rsl.setDeleted(mapPrev.size());
         rsl.setAdded(tempCur.size());
         return rsl;
     }
