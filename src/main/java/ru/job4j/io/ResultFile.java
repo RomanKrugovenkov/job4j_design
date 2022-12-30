9bd4af45fcd414d808863e25a0e0dfda243cca29
krugo <krugovenkov@mail.ru>
2022-12-30 20:00:06 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.util.Arrays;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            int size = 10;
+            int[][] array = new int[size][size];
+            for (int row = 0; row < size; row++) {
+                for (int cell = 0; cell < size; cell++) {
+                    array[row][cell] = (row + 1) * (cell + 1);
+                }
+                out.write(Arrays.toString(array[row]).getBytes());
+                out.write(System.lineSeparator().getBytes());
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
