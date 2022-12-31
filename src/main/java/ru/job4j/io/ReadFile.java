9490835549109daeb58da622fab510a702ef19ef
krugo <krugovenkov@mail.ru>
2022-12-31 21:02:36 +0300

    0.4. BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -4,6 +4,14 @@ import java.io.*;
 
 public class ReadFile {
     public static void main(String[] args) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("result.txt")
+                ))) {
+            out.println("Hello, world!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         try (BufferedReader in = new BufferedReader(new FileReader("input.txt"))) {
             in.lines().forEach(System.out::println);
         } catch (Exception e) {
