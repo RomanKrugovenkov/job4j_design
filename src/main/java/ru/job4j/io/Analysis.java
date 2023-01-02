64669af5e39c1a25068fa836302cdb75484df2e3
krugo <krugovenkov@mail.ru>
2023-01-02 15:48:26 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("./data/server.log", "./data/target.csv");
+    }
+
+    public void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             BufferedWriter out = new BufferedWriter(new FileWriter(target))) {
+            String line;
+            String offTime = null;
+            while ((line = in.readLine()) != null) {
+                var strArray = line.split(" ", 2);
+                if (offTime == null && ("400".equals(strArray[0]) || "500".equals(strArray[0]))) {
+                    offTime = strArray[1];
+                } else if (offTime != null && "200".equals(strArray[0]) || "300".equals(strArray[0])) {
+                    out.write(offTime + ";" + strArray[1] + ";");
+                    out.newLine();
+                    offTime = null;
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
