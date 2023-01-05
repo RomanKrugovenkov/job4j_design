d7bf343671ac71ec6d4c6645e87fc685003ff816
krugo <krugovenkov@mail.ru>
2023-01-05 11:07:26 +0300

    5. Валидация параметров запуска. [#246865 #377508]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,11 +1,14 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Search {
     public static void main(String[] args) throws IOException {
@@ -15,8 +18,20 @@ public class Search {
     }
 
     public static void validation(String[] args) {
-        if (args.length == 0) {
+        if (args.length < 2) {
             throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        Pattern pattern = Pattern.compile("\\.\\S{1,}");
+        String extension = args[1];
+        if (!pattern.matcher(extension).find()) {
+            throw new IllegalArgumentException(String.format("Not file extension \"%s\"", extension));
         }
     }
 
