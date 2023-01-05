46a990ad368c568e1de9a510f2c60ee5421acddd
krugo <krugovenkov@mail.ru>
2023-01-05 10:25:12 +0300

    Поиск дубликатов [#315066 #377499]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -1,23 +1,25 @@
 package ru.job4j.io.duplicates;
 
-import ru.job4j.io.SearchFiles;
 import java.io.IOException;
-import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.Map;
 
 public class DuplicatesFinder {
 
     public static void main(String[] args) throws IOException {
-        duplicatesFinder(Path.of("C:\\projects\\job4j_design\\src\\main\\java\\ru\\job4j\\io\\duplicates"));
+        Path path = Path.of("./");
+        printMap(duplicatesFinder(path));
     }
 
-    public static void duplicatesFinder(Path root) throws IOException {
+    public static Map<FileProperty, List<String>> duplicatesFinder(Path root) throws IOException {
         DuplicatesVisitor duplVisitor = new DuplicatesVisitor();
         Files.walkFileTree(root, duplVisitor);
-        var map = duplVisitor.getMap();
+        return duplVisitor.getMap();
+    }
+
+    public static void printMap(Map<FileProperty, List<String>> map) {
         for (var file : map.entrySet()) {
             if (file.getValue().size() > 1) {
                 System.out.printf("%s - %dByte%n", file.getKey().getName(), file.getKey().getSize());
