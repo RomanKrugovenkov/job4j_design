9bbe72336ecdaabb71e47d8442d211e9c8480938
krugo <krugovenkov@mail.ru>
2023-01-13 21:48:45 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -13,5 +13,18 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        String name = "Petr Arsentev";
+        char character = '*';
+        boolean bool = false;
+        byte byteNum = 7;
+        short shortNum = 12;
+        int intNum = 789;
+        long longNum = 123456;
+        float floatNum = 123.1f;
+        double doubleNum = 456.123d;
+        LOG.debug("User info name : {}, symbol : {}", name, character);
+        LOG.info("info message : status {}, byteNum : {}", bool, byteNum);
+        LOG.warn("warn message : shortNum {}, intNum : {}", shortNum, intNum);
+        LOG.error("error message : longNum {}, floatNum : {}, doubleNum : {}", longNum, floatNum, doubleNum);
     }
 }
