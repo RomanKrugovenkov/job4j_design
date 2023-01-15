ecbcc510a0645856ee062aad1ea36d1ac9583ee7
krugo <krugovenkov@mail.ru>
2023-01-15 11:09:35 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/json/Car.java
+++ src/main/java/ru/job4j/serialization/json/Car.java
@@ -1,11 +1,28 @@
 package ru.job4j.serialization.json;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Arrays;
+
+@XmlRootElement(name = "Car")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Car {
-    private final boolean credit;
-    private final int speed;
-    private final String name;
-    private final Contact contact;
-    private final String[] options;
+
+    @XmlAttribute
+    private boolean credit;
+    private int speed;
+    private String name;
+    private Contact contact;
+    @XmlElementWrapper(name = "options")
+    @XmlElement(name = "option")
+    private String[] options;
+
+    public Car() { }
 
     public Car(boolean credit, int speed, String name, Contact contact, String[] options) {
         this.credit = credit;
@@ -15,12 +32,39 @@ public class Car {
         this.options = options;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JAXBException {
         Car car = new Car(
                 true,
                 200,
                 "Nissan",
                 new Contact("22-222"),
                 new String[] {"mats", "tyres"});
+        JAXBContext context = JAXBContext.newInstance(Car.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String result = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(car, writer);
+            result = writer.getBuffer().toString();
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(result)) {
+            Car resultCar = (Car) unmarshaller.unmarshal(reader);
+            System.out.println(resultCar);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "credit=" + credit
+                + ", speed=" + speed
+                + ", name=" + name
+                + ", contact=" + contact
+                + ", options=" + Arrays.toString(options)
+                + '}';
     }
 }
