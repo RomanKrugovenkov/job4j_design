d372d8862d68eae4c821eeae6ef6239c9c14a43f
krugo <krugovenkov@mail.ru>
2023-03-25 17:24:57 +0300

    5. Курсор в SQL [#504816]

---
+++ DB/cursor_back.sql
@@ -0,0 +1,23 @@
+BEGIN;
+
+DECLARE
+cursor_products cursor for
+select *
+from products;
+
+--переход и получение 20 строки
+MOVE FORWARD 19 FROM cursor_products;
+FETCH NEXT FROM cursor_products;
+
+--переход и получение 15 строки
+move backward 4 from cursor_products;
+fetch prior from cursor_products;
+
+--переход и получение 7 строки
+move backward 7 from cursor_products;
+fetch prior from cursor_products;
+
+--переход и получение 2 и 1 строки
+move backward 4 from cursor_products;
+fetch prior from cursor_products;
+fetch prior from cursor_products;
