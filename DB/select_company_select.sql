be88253bdc4e8453e9e58e545df763c260ec126f
krugo <krugovenkov@mail.ru>
2023-05-29 11:01:15 +0300

    3. SQL Select by company [#2760]

--- DB/select_company_select.sql
+++ DB/select_company_select.sql
@@ -7,3 +7,15 @@ where NOT p.company_id = 5;
 select p.name, c.name
 from person p
          join company c on c.id = p.company_id;
+
+-- выбрать название компании с макс количеством человек + количество человек в этой компании
+select c.name, count(p.company_id) as count_person
+from company c
+         join person p on c.id = p.company_id
+group by c.name
+having count(p.company_id) =
+       (select count(company_id) as c_c
+        from person p
+        group by company_id
+        order by c_c desc
+        limit 1);
