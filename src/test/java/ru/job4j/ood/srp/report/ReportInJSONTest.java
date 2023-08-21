a5d35acacc262a3652fbaf3a2342020fa08648de
krugo <krugovenkov@mail.ru>
2023-08-21 20:28:50 +0300

    1. Поддержка форматов XML, JSON в генераторе отчетов. [#851 #438000]

--- src/test/java/ru/job4j/ood/srp/report/ReportInJSONTest.java
+++ src/test/java/ru/job4j/ood/srp/report/ReportInJSONTest.java
@@ -6,7 +6,9 @@ import ru.job4j.ood.srp.formatter.ReportDateTimeParser;
 import ru.job4j.ood.srp.model.Employee;
 import ru.job4j.ood.srp.store.MemStore;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,25 +22,28 @@ class ReportInJSONTest {
         DateTimeParser<Calendar> parser = new ReportDateTimeParser();
         store.add(worker1);
         Report reportInJSON = new ReportInJSON(store, parser);
-        StringBuilder expect = new StringBuilder()
-                .append("[")
-                .append("{\"name\":\"").append(worker1.getName()).append("\",")
-                .append("\"hired\":{\"year\":").append(worker1.getHired().get(Calendar.YEAR)).append(",")
-                .append("\"month\":").append(worker1.getHired().get(Calendar.MONTH)).append(",")
-                .append("\"dayOfMonth\":").append(worker1.getHired().get(Calendar.DAY_OF_MONTH)).append(",")
-                .append("\"hourOfDay\":").append(worker1.getHired().get(Calendar.HOUR)).append(",")
-                .append("\"minute\":").append(worker1.getHired().get(Calendar.MINUTE)).append(",")
-                .append("\"second\":").append(worker1.getHired().get(Calendar.SECOND)).append("},")
-                .append("\"fired\":{\"year\":").append(worker1.getFired().get(Calendar.YEAR)).append(",")
-                .append("\"month\":").append(worker1.getFired().get(Calendar.MONTH)).append(",")
-                .append("\"dayOfMonth\":").append(worker1.getFired().get(Calendar.DAY_OF_MONTH)).append(",")
-                .append("\"hourOfDay\":").append(worker1.getFired().get(Calendar.HOUR)).append(",")
-                .append("\"minute\":").append(worker1.getFired().get(Calendar.MINUTE)).append(",")
-                .append("\"second\":").append(worker1.getFired().get(Calendar.SECOND)).append("},")
-                .append("\"salary\":")
-                .append(worker1.getSalary()).append("}")
-                .append("]");
+        String expect = String.format(Locale.ROOT,
+                "[{\"name\":\"%s\",\"hired\":{\"year\":%s,\"month\":%s,\"dayOfMonth\":%s,\"hourOfDay\":%s,\"minute\":%s,\"second\":%s},"
+                        + "\"fired\":{\"year\":%s,\"month\":%s,\"dayOfMonth\":%s,\"hourOfDay\":%s,\"minute\":%s,\"second\":%s},\"salary\":%.1f}]",
+                worker1.getName(),
+                worker1.getHired().get(Calendar.YEAR),
+                worker1.getHired().get(Calendar.MONTH),
+                worker1.getHired().get(Calendar.DAY_OF_MONTH),
+                worker1.getHired().get(Calendar.HOUR_OF_DAY),
+                worker1.getHired().get(Calendar.MINUTE),
+                worker1.getHired().get(Calendar.SECOND),
+                worker1.getFired().get(Calendar.YEAR),
+                worker1.getFired().get(Calendar.MONTH),
+                worker1.getFired().get(Calendar.DAY_OF_MONTH),
+                worker1.getFired().get(Calendar.HOUR_OF_DAY),
+                worker1.getFired().get(Calendar.MINUTE),
+                worker1.getFired().get(Calendar.SECOND),
+                worker1.getSalary());
         System.out.println(expect);
-        assertThat(reportInJSON.generate(em -> true)).isEqualTo(expect.toString());
+
+        String pattern = "'\"year\":'yyyy',\"month\":'M',\"dayOfMonth\":'dd',\"hourOfDay\":'H',\"minute\":'m',\"second\":'ss";
+        System.out.println(new SimpleDateFormat(pattern).format(worker1.getHired().getTime()));
+
+        assertThat(reportInJSON.generate(em -> true)).isEqualTo(expect);
     }
 }
